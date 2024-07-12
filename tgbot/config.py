a6beac890f1676ca3ef70a40665a814732ec96f1
LexinPro <frolovu301@gmail.com>
2024-07-12 12:37:07 +0300

    Create project

---
+++ tgbot/config.py
@@ -0,0 +1,16 @@
+import os
+
+class ConfigBot:
+    TOKEN = os.environ.get("TOKEN")
+
+    OWNER = {
+        "ID": 0,
+        "USERNAME": "frolovtg2005"
+    }
+
+    ADMINS = [
+        {
+            "ID": 0,
+            "USERNAME": "frolovtg2005"
+        },
+    ]
---
+++ tgbot/mybot.py
@@ -0,0 +1,4 @@
+from telebot import TeleBot
+from tgbot.config import ConfigBot
+
+bot = TeleBot(ConfigBot.TOKEN)
